uk-gov-mirror/alphagov.notifications-admin

Convert radios on /support page (basic)

app/main/forms.py
@@ -1285,7 +1285,7 @@ class CreateKeyForm(StripWhitespaceForm):
 
 
 class SupportType(StripWhitespaceForm):
-    support_type = RadioField(
+    support_type = govukRadioField(
         'How can we help you?',
         choices=[
             (PROBLEM_TICKET_TYPE, 'Report a problem'),
@@ -1295,12 +1295,15 @@ class SupportType(StripWhitespaceForm):
 
 
 class SupportRedirect(StripWhitespaceForm):
-    who = RadioField(
+    who = govukRadioField(
         'What do you need help with?',
         choices=[
             ('public-sector', 'I work in the public sector and need to send emails, text messages or letters'),
             ('public', 'I’m a member of the public with a question for the government'),
         ],
+        param_extensions={
+            "fieldset": {"legend": {"classes": "govuk-visually-hidden"}}
+        }
     )
 
 

GSA/notifications-admin

app/main/forms.py
@@ -1516,7 +1516,7 @@ class CreateKeyForm(StripWhitespaceForm):
 
 
 class SupportType(StripWhitespaceForm):
-    support_type = RadioField(
+    support_type = GovukRadiosField(
         'How can we help you?',
         choices=[
             (PROBLEM_TICKET_TYPE, 'Report a problem'),
@@ -1526,12 +1526,15 @@ class SupportType(StripWhitespaceForm):
 
 
 class SupportRedirect(StripWhitespaceForm):
-    who = RadioField(
+    who = GovukRadiosField(
         'What do you need help with?',
         choices=[
             ('public-sector', 'I work in the public sector and need to send emails, text messages or letters'),
             ('public', 'I’m a member of the public with a question for the government'),
         ],
+        param_extensions={
+            "fieldset": {"legend": {"classes": "govuk-visually-hidden"}}
+        }
     )
 
 