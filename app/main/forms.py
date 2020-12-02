uk-gov-mirror/alphagov.notifications-admin

Convert radios for P.Admin email branding (basic)

app/main/forms.py
@@ -1517,7 +1517,7 @@ class ServiceUpdateEmailBranding(StripWhitespaceForm):
         ]
     )
     file = FileField_wtf('Upload a PNG logo', validators=[FileAllowed(['png'], 'PNG Images only!')])
-    brand_type = RadioField(
+    brand_type = govukRadioField(
         "Brand type",
         choices=[
             ('both', 'GOV.UK and branding'),

GSA/notifications-admin

app/main/forms.py
@@ -1753,7 +1753,7 @@ class ServiceUpdateEmailBranding(StripWhitespaceForm):
         }
     )
     file = FileField_wtf('Upload a PNG logo', validators=[FileAllowed(['png'], 'PNG Images only!')])
-    brand_type = RadioField(
+    brand_type = GovukRadiosField(
         "Brand type",
         choices=[
             ('both', 'GOV.UK and branding'),