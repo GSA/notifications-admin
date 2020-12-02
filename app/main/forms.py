uk-gov-mirror/alphagov.notifications-admin

Convert radios on uploads preview page (basic)

app/main/forms.py
@@ -1183,7 +1183,7 @@ class LetterUploadPostageForm(StripWhitespaceForm):
     def show_postage(self):
         return len(self.postage.choices) > 1
 
-    postage = RadioField(
+    postage = govukRadioField(
         'Choose the postage for this letter',
         choices=[
             ('first', 'First class post'),

GSA/notifications-admin

app/main/forms.py
@@ -1414,7 +1414,7 @@ class LetterUploadPostageForm(StripWhitespaceForm):
     def show_postage(self):
         return len(self.postage.choices) > 1
 
-    postage = RadioField(
+    postage = GovukRadiosField(
         'Choose the postage for this letter',
         choices=[
             ('first', 'First class post'),