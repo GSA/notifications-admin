6c60183a1082bd407a1d532e9476bd1624a14b92
James Moffet <jimmoffet@gmail.com>
2022-07-19 18:49:05 -0700

    delete log

--- app/notify_client/status_api_client.py
+++ app/notify_client/status_api_client.py
@@ -9,19 +9,9 @@ class StatusApiClient(NotifyAdminAPIClient):
 
     # @cache.set('live-service-and-organisation-counts', ttl_in_seconds=3600)
     def get_count_of_live_services_and_organisations(self):
-        current_app.logger.info("Getting count of live services and organisations")
-        current_app.logger.info("Redis url from config is: {}".format(current_app.config['REDIS_URL']))
-        current_app.logger.info("Redis url from env is: {}".format( os.environ.get('REDIS_URL') ))
-        current_app.logger.info("Redis enabled from config is: {}".format(current_app.config['REDIS_ENABLED']))
-        current_app.logger.info("Redis enabled from env is: {}".format( os.environ.get('REDIS_ENABLED') ))
         return self.get(url='/_status/live-service-and-organisation-counts')
 
     def get_services(self):
-        current_app.logger.info("Getting count of live services and organisations")
-        current_app.logger.info("Redis url from config is: {}".format(current_app.config['REDIS_URL']))
-        current_app.logger.info("Redis url from env is: {}".format( os.environ.get('REDIS_URL') ))
-        current_app.logger.info("Redis enabled from config is: {}".format(current_app.config['REDIS_ENABLED']))
-        current_app.logger.info("Redis enabled from env is: {}".format( os.environ.get('REDIS_ENABLED') ))
         return self.get(url='/service')
 
 
