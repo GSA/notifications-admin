4074273d2fb72bb5cdb61a68c7a0ae470d0c9fde
jimmoffet <jim.moffet@gsa.gov>
2022-08-24 16:10:48 -0700

    redis health check

--- app/status/views/healthcheck.py
+++ app/status/views/healthcheck.py
@@ -20,3 +20,20 @@ def show_status():
             api=api_status,
             git_commit=version.__git_commit__,
             build_time=version.__time__), 200
+
+
+@status.route('/_status/redis', methods=['GET'])
+def show_redis_status():
+    if request.args.get('elb', None) or request.args.get('simple', None):
+        return jsonify(status="ok"), 200
+    else:
+        try:
+            api_status = status_api_client.get_count_of_live_services_and_organisations_cached()
+        except HTTPError as e:
+            current_app.logger.exception("API failed to respond")
+            return jsonify(status="error", message=str(e.message)), 500
+        return jsonify(
+            status="ok",
+            api=api_status,
+            git_commit=version.__git_commit__,
+            build_time=version.__time__), 200
