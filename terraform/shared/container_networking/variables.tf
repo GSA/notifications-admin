d88654e4d800d38f3aaf661a1e285bdba541417c
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-09-27 11:07:30 -0400

    Add comment linking to cloud.gov networking docs

--- terraform/shared/container_networking/variables.tf
+++ terraform/shared/container_networking/variables.tf
@@ -9,5 +9,7 @@ variable "source_app_name" {}
 variable "destination_app_name" {}
 variable "destination_port" {
   type    = string
+  # 61443 is the port to use to enable automatic TLS termination as specified at
+  # https://cloud.gov/docs/management/container-to-container/#configuring-secure-container-to-container-networking
   default = "61443"
 }
