eaf3681ae1a40771407fd96c38bcc3423e4a338f
Jonathan Bobel <jbobel@flexion.us>
2023-04-24 14:57:35 -0400

    Initial USWDS install

--- gulpfile.js
+++ gulpfile.js
@@ -257,3 +257,32 @@ exports.lint = series(lint.sass, lint.js);
 
 // Optional: recompile on changes
 exports.watch = series(defaultTask, watchForChanges);
+
+
+// 3. Compile USWDS
+/**
+* Import uswds-compile
+*/
+const uswds = require("@uswds/compile");
+
+/**
+* USWDS version
+* Set the major version of USWDS you're using
+* (Current options are the numbers 2 or 3)
+*/
+uswds.settings.version = 3;
+
+/**
+* Path settings
+* Set as many as you need
+*/
+uswds.paths.dist.css = './app/static/css';
+uswds.paths.dist.theme = './app/assets/sass/uswds';
+
+/**
+* Exports
+* Add as many as you need
+*/
+exports.init = uswds.init;
+exports.compile = uswds.compile;
+exports.watch = uswds.watch;
