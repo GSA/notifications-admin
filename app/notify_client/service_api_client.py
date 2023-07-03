faf0c9c62973f291242ab26cd99d163137a5b6db
Kenneth Kehl <@kkehl@flexion.us>
2023-07-03 11:43:16 -0700

    code review feedback

--- app/notify_client/service_api_client.py
+++ app/notify_client/service_api_client.py
@@ -190,7 +190,7 @@ class ServiceAPIClient(NotifyAdminAPIClient):
     @cache.delete('service-{service_id}-templates')
     @cache.delete_by_pattern('service-{service_id}-template-*')
     def update_service_template(
-        self, id_, name, type_, content, service_id, subject=None, process_type=None
+        self, id_, name, type_, content, service_id, subject=None
     ):
         """
         Update a service template.
@@ -206,10 +206,9 @@ class ServiceAPIClient(NotifyAdminAPIClient):
             data.update({
                 'subject': subject
             })
-        if process_type:
-            data.update({
-                'process_type': process_type
-            })
+        data.update({
+            'process_type': 'normal'
+        })
         data = _attach_current_user(data)
         endpoint = "/service/{0}/template/{1}".format(service_id, id_)
         return self.post(endpoint, data)
