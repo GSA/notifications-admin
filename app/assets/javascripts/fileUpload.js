dedb41f67e269eb916906bf5ad32ad377d7ad7e5
Jonathan Bobel <jbobel@flexion.us>
2023-09-15 12:26:31 -0400

    Fixing styling issues on the send message flow

--- app/assets/javascripts/fileUpload.js
+++ app/assets/javascripts/fileUpload.js
@@ -16,7 +16,7 @@
       // The label gets styled like a button and is used to hide the native file upload control. This is so that
       // users see a button that looks like the others on the site.
 // 
-      this.$form.find('label.file-upload-button').addClass('usa-button');
+      this.$form.find('label.file-upload-button').addClass('usa-button margin-bottom-1');
 
       // Clear the form if the user navigates back to the page
       $(window).on("pageshow", () => this.$form[0].reset());
