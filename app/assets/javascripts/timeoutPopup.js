57f2ee798be22a9ff2316e34d324b43ba63270cd
Carlo Costino <carlo.costino@gsa.gov>
2023-09-28 15:31:08 -0400

    Made several adjustments for session timer tests
    - Turned the timeoutPopup module into a proper module that is exported
    - Pulled out the setInterval inner function to be its own method
    - Adjusted the session timer tests to have additional setup and teardown pieces
    - Added a few tests
    
    There may be some additional adjustments needed, e.g., we may not have to expose all of the methods, but then we have to figure out how to make them accessible in the tests another way.
    
    h/t @A-Shumway42 for pairing with me to review all of these changes and approach!
    
    Signed-off-by: Carlo Costino <carlo.costino@gsa.gov>

--- app/assets/javascripts/timeoutPopup.js
+++ app/assets/javascripts/timeoutPopup.js
@@ -1,25 +1,23 @@
 (function(global) {
     "use strict";
+
     const sessionTimer = document.getElementById("sessionTimer");
 
-    setTimeout(function() {
-        var timeTillSessionEnd = new Date().getTime() + (5 * 60 * 1000);
-        var x = setInterval(function() {
-            var now = new Date().getTime();
-            var difference = timeTillSessionEnd - now;
-            var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((difference % (1000 * 60)) / 1000);
-            document.getElementById("timeLeft").innerHTML = + minutes + "m " + seconds + "s";
-            showTimer();
-            document.getElementById("logOutTimer").addEventListener("click", logoutUser);
-            document.getElementById("extendSessionTimer").addEventListener("click", extendSession);
-            if (difference < 0) {
-                clearInterval(x);
-                closeTimer();
-                logoutUser();
-            }
-        }, 1000);
-    }, 25 * 60 * 1000);
+    function checkTimer(timeTillSessionEnd) {
+        var now = new Date().getTime();
+        var difference = timeTillSessionEnd - now;
+        var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+        document.getElementById("timeLeft").innerHTML = + minutes + "m " + seconds + "s";
+        showTimer();
+        document.getElementById("logOutTimer").addEventListener("click", logoutUser);
+        document.getElementById("extendSessionTimer").addEventListener("click", extendSession);
+        if (difference < 0) {
+            clearInterval(x);
+            closeTimer();
+            logoutUser();
+        }
+    }
 
     function logoutUser() {
         window.location.href = '/sign-out';
@@ -36,9 +34,17 @@
     function closeTimer() {
         sessionTimer.close();
     }
-    })(window);
 
+    global.GOVUK.Modules.TimeoutPopup = function() {
+        setTimeout(function() {
+            var timeTillSessionEnd = new Date().getTime() + (5 * 60 * 1000);
+            var x = setInterval(checkTimer, 1000, timeTillSessionEnd);
+        }, 25 * 60 * 1000);
+    };
 
-
-
-
+    global.GOVUK.Modules.TimeoutPopup.checkTimer = checkTimer;
+    global.GOVUK.Modules.TimeoutPopup.logoutUser = logoutUser;
+    global.GOVUK.Modules.TimeoutPopup.extendSession = extendSession;
+    global.GOVUK.Modules.TimeoutPopup.showTimer = showTimer;
+    global.GOVUK.Modules.TimeoutPopup.closeTimer = closeTimer;
+})(window);
