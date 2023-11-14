d1682c641d6052d204c3b22044f2beedc992c6e4
Kenneth Kehl <@kkehl@flexion.us>
2023-11-14 07:14:27 -0800

    fix flake 8

--- app/main/views/sign_out.py
+++ app/main/views/sign_out.py
@@ -11,7 +11,6 @@ from app.main import main
 # ask Tim Donaworth the same for #2
 
 
-
 def _sign_out_at_login_dot_gov():
     base_url = "https://idp.int.identitysandbox.gov/openid_connect/logout?"
     client_id = f"client_id={os.getenv('LOGIN_DOT_GOV_CLIENT_ID')}"
@@ -33,12 +32,16 @@ def sign_out():
     if current_user.is_authenticated:
         # TODO This doesn't work yet, due to problems above.
         current_user.sign_out()
-    
-        return redirect("https://idp.int.identitysandbox.gov/openid_connect/logout?client_id=urn:gov:gsa:openidconnect.profiles:sp:sso:gsa:test_notify_gov&post_logout_redirect_uri=http://localhost:6012/sign-out")
+
+        return redirect(
+            "https://idp.int.identitysandbox.gov/openid_connect/logout?client_id=urn:gov:gsa:openidconnect.profiles:sp:sso:gsa:test_notify_gov&post_logout_redirect_uri=http://localhost:6012/sign-out"  # noqa
+        )
     return redirect(url_for("main.index"))
 
 
-@main.route("/sign-out-at-login-gov", methods=(["POST"]))
-def sign_out_at_login_gov():
-    current_app.logger.info("SHOULD BE REDIRECTING TO LOGIN GOV")
-    return redirect("https://idp.int.identitysandbox.gov/openid_connect/logout?client_id=urn:gov:gsa:openidconnect.profiles:sp:sso:gsa:test_notify_gov&post_logout_redirect_uri=http://localhost:6012/sign-out")
+# @main.route("/sign-out-at-login-gov", methods=(["POST"]))
+# def sign_out_at_login_gov():
+#     current_app.logger.info("SHOULD BE REDIRECTING TO LOGIN GOV")
+#     return redirect(
+#         "https://idp.int.identitysandbox.gov/openid_connect/logout?client_id=urn:gov:gsa:openidconnect.profiles:sp:sso:gsa:test_notify_gov&post_logout_redirect_uri=http://localhost:6012/sign-out"
+#    )
