f8b8dd5fb3018f428c57563d08d1b9e187347c5b
Carlo Costino <carlo.costino@gsa.gov>
2024-02-26 17:59:18 -0500

    Fix broken test and include note about why
    Signed-off-by: Carlo Costino <carlo.costino@gsa.gov>

--- tests/app/main/views/test_sign_in.py
+++ tests/app/main/views/test_sign_in.py
@@ -21,8 +21,10 @@ def test_render_sign_in_template_for_new_user(client_request):
     # Removing for the pilot
     # assert page.select('main a')[0].text == 'create one now'
     # assert page.select('main a')[0]['href'] == url_for('main.register')
-    assert page.select("main a")[1].text == "Forgot your password?"
-    assert page.select("main a")[1]["href"] == url_for("main.forgot_password")
+    # TODO:  Fix this test to be less brittle! If the Login.gov link is enabled,
+    #        then these indices need to be 1 instead of 0.
+    assert page.select("main a")[0].text == "Forgot your password?"
+    assert page.select("main a")[0]["href"] == url_for("main.forgot_password")
     assert "Sign in again" not in normalize_spaces(page.text)
 
 
