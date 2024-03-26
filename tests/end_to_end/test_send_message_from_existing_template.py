5e16f210d8ca00fa8d97e3e4420d5fb0654d0df3
Kenneth Kehl <@kkehl@flexion.us>
2024-03-26 08:32:37 -0700

    only test chromium right now

--- tests/end_to_end/test_send_message_from_existing_template.py
+++ tests/end_to_end/test_send_message_from_existing_template.py
@@ -209,6 +209,8 @@ def handle_existing_template_case(page):
     # Check to make sure that we've arrived at the next page.
     page.wait_for_load_state("domcontentloaded")
 
+    print(page.content())
+
     use_this_template_button = page.get_by_text("Use this template")
     expect(use_this_template_button).to_be_visible()
     use_this_template_button.click()
