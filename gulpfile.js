b759f8a87fce919014d1be740f3a286ecf570b90
Jonathan Bobel <jonathan.bobel@gsa.gov>
2024-06-07 09:44:30 -0400

    Adding js to node_modules instead of the head

--- gulpfile.js
+++ gulpfile.js
@@ -100,7 +100,9 @@ const javascripts = () => {
       paths.npm + 'query-command-supported/dist/queryCommandSupported.min.js',
       paths.npm + 'timeago/jquery.timeago.js',
       paths.npm + 'textarea-caret/index.js',
-      paths.npm + 'cbor-js/cbor.js'
+      paths.npm + 'cbor-js/cbor.js',
+      paths.npm + 'socket.io-client/dist/socket.io.min.js',
+      paths.npm + 'chart.js/dist/chart.umd.js'
     ]));
 
   // JS local to this application
