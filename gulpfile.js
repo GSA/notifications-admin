9d22b99e9b3f6f28022f60bae331442b3d51c522
Jonathan Bobel <jonathan.bobel@gsa.gov>
2024-06-10 10:20:33 -0400

    Updates after merge

--- gulpfile.js
+++ gulpfile.js
@@ -131,7 +131,7 @@ const javascripts = () => {
     paths.src + 'javascripts/date.js',
     paths.src + 'javascripts/loginAlert.js',
     paths.src + 'javascripts/main.js',
-    paths.src + 'javascripts/sampleChartDashboard.js',
+    paths.src + 'javascripts/chartDashboard.js',
   ])
     .pipe(plugins.prettyerror())
     .pipe(plugins.babel({
