4982452becec60b0f3c70717ecb9f61d3e0bb9a7
John Skiles Skinner <john.skinner@gsa.gov>
2024-07-26 11:28:03 -0700

    Remove the deprecated arg from the domain code

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -69,7 +69,6 @@ module "domain" {
   cf_space_name    = local.cf_space_name
   app_name_or_id   = "${local.app_name}-${local.env}"
   name             = "${local.app_name}-domain-${local.env}"
-  recursive_delete = false
   cdn_plan_name    = "domain"
   domain_name      = "beta.notify.gov"
 }
