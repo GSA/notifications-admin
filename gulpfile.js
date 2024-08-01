9471174f20c58b8e89d6823e40d64bf8c7518d0a
Beverly Nguyen <Beverly.Nguyen@gsa.gov>
2024-08-01 15:44:05 -0700

    removed extra template path in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,6 @@ plugins.uglify = require('gulp-uglify');
 const paths = {
   src: 'app/assets/',
   dist: 'app/static/',
-  templates: 'app/templates/',
   npm: 'node_modules/',
   toolkit: 'node_modules/govuk_frontend_toolkit/',
   govuk_frontend: 'node_modules/govuk-frontend/'
