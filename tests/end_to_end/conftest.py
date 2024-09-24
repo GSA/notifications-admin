99ce42fc4ac499fbb1768769e8ac789f1e821618
Kenneth Kehl <@kkehl@flexion.us>
2024-09-24 08:44:01 -0700

    revert test setting to ignore moderate failures

--- tests/end_to_end/conftest.py
+++ tests/end_to_end/conftest.py
@@ -29,12 +29,10 @@ def check_axe_report(page):
 
     results = axe.run(page)
 
-    # TODO we are setting this to critical for now
-    # to keep tests passing.  Once the serious and
-    # moderate issues are fixed, we will set this
-    # 'moderate'
+    # TODO fix remaining 'moderate' failures
+    # so we can set the level we skip to minor only
     for violation in results["violations"]:
         assert violation["impact"] in [
             "minor",
-            # "moderate",
+            "moderate",
         ], f"Accessibility violation: {violation}"
