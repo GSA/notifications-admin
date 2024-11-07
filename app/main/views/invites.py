923a345d19442c157b0e22a4092f25dcccf7c712
Cliff Hill <clifford.hill@gsa.gov>
2024-11-07 11:56:03 -0500

    Fixed tests.
    Signed-off-by: Cliff Hill <clifford.hill@gsa.gov>

--- app/main/views/invites.py
+++ app/main/views/invites.py
@@ -38,9 +38,9 @@ def accept_invite(token):
         )
     if invited_user.status == "accepted":
         session.pop("invited_user_id", None)
-        service = Service.from_id(invited_user.service_id)
+        service = Service.from_id(invited_user.service)
         return redirect(
-            url_for("main.service_dashboard", service_id=invited_user.service_id)
+            url_for("main.service_dashboard", service_id=invited_user.service)
         )
 
     session["invited_user_id"] = invited_user.id
