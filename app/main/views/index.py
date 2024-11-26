2fa9c1ed7dd1d98ee9cf3bb544e98de8a3ee4a48
Beverly Nguyen <Beverly.Nguyen@gsa.gov>
2024-11-26 12:24:06 -0800

    removed unnecessary route

--- app/main/views/index.py
+++ app/main/views/index.py
@@ -293,7 +293,6 @@ def benchmark_performance():
 
 
 @main.route("/using-notify/guidance")
-@main.route("/guides/using-notify/guidance")
 @user_is_logged_in
 def guidance_index():
     return render_template(
