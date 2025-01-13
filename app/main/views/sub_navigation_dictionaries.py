52791cab7b4a3f9dcf59c50167cbdebf386ce0c7
alexjanousekGSA <alex.janousek@gsa.gov>
2025-01-13 14:47:41 -0500

    Updated navigation links and backstopjs

--- app/main/views/sub_navigation_dictionaries.py
+++ app/main/views/sub_navigation_dictionaries.py
@@ -1,46 +1,11 @@
-def features_nav():
-    return [
-        {
-            "name": "Features",
-            "link": "main.features",
-            "sub_navigation_items": [
-                # {
-                #     "name": "Text messages",
-                #     "link": "main.features_sms",
-                # },
-            ],
-        },
-        {
-            "name": "Roadmap",
-            "link": "main.roadmap",
-        },
-        {
-            "name": "Security",
-            "link": "main.security",
-        },
-    ]
-
-
 def using_notify_nav():
     nav_items = [
         {"name": "Get started", "link": "main.get_started"},
-        {"name": "Guides", "link": "main.best_practices"},
-        {"name": "Trial mode", "link": "main.trial_mode_new"},
-        {"name": "Tracking usage", "link": "main.pricing"},
-        {"name": "Delivery Status", "link": "main.message_status"},
-        {"name": "Guidance", "link": "main.guidance_index"},
-    ]
-
-    return nav_items
-
-
-def best_practices_nav():
-    return [
         {
             "name": "Best Practices",
             "link": "main.best_practices",
-        },
-        {
+            "sub_navigation_items": [
+                {
             "name": "Clear goals",
             "link": "main.clear_goals",
         },
@@ -51,16 +16,6 @@ def best_practices_nav():
         {
             "name": "Establish trust",
             "link": "main.establish_trust",
-            "sub_navigation_items": [
-                {
-                    "name": "Get the word out",
-                    "link": "main.establish_trust#get-the-word-out",
-                },
-                {
-                    "name": "As people receive texts",
-                    "link": "main.establish_trust#as-people-receive-texts",
-                },
-            ],
         },
         {
             "name": "Write for action",
@@ -73,8 +28,16 @@ def best_practices_nav():
         {
             "name": "Benchmark performance",
             "link": "main.benchmark_performance",
+        }
+            ]
         },
+        {"name": "Trial mode", "link": "main.trial_mode_new"},
+        {"name": "Tracking usage", "link": "main.pricing"},
+        {"name": "Delivery Status", "link": "main.message_status"},
+        {"name": "Guidance", "link": "main.guidance_index"},
     ]
+
+    return nav_items
 
 
 def about_notify_nav():
