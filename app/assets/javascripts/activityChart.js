2159a9ddc51e20af805b7ebcccfcdec2ea3e9610
Beverly Nguyen <Beverly.Nguyen@gsa.gov>
2025-01-14 11:48:23 -0800

    clean up

--- app/assets/javascripts/activityChart.js
+++ app/assets/javascripts/activityChart.js
@@ -235,7 +235,6 @@
                         pendingData.push(data[dateString].sms.pending || 0);
                         totalMessages += data[dateString].sms.delivered + data[dateString].sms.failure + data[dateString].sms.pending;
 
-                        // Calculate the total number of messages
                         totalMessages += data[dateString].sms.delivered + data[dateString].sms.failure;
                     }
                 }
