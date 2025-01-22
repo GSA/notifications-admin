aa03fc1cc02a09c94f215c8e6c1a84ab025cb3c0
Beverly Nguyen <Beverly.Nguyen@gsa.gov>
2025-01-22 10:55:42 -0800

    Sentence case

--- app/main/views/sub_navigation_dictionaries.py
+++ app/main/views/sub_navigation_dictionaries.py
@@ -2,7 +2,7 @@ def using_notify_nav():
     nav_items = [
         {"name": "Get started", "link": "main.get_started"},
         {
-            "name": "Best Practices",
+            "name": "Best practices",
             "link": "main.best_practices",
             "sub_navigation_items": [
                 {
@@ -33,7 +33,7 @@ def using_notify_nav():
         },
         {"name": "Trial mode", "link": "main.trial_mode_new"},
         {"name": "Tracking usage", "link": "main.pricing"},
-        {"name": "Delivery Status", "link": "main.message_status"},
+        {"name": "Delivery status", "link": "main.message_status"},
         {"name": "How to", "link": "main.how_to"},
     ]
 
