8dfa881f260a4bb41656c1470e165a5c9d153a95
Beverly Nguyen <Beverly.Nguyen@gsa.gov>
2025-04-17 11:48:55 -0700

    fix test

--- app/main/views/index.py
+++ app/main/views/index.py
@@ -25,10 +25,8 @@ from app.utils.user import user_is_logged_in
 def check_feature_flags():
     # Placeholder for future feature flag checks
     # Example:
-    if "/jobs" in request.path and not current_app.config.get(
-        "FEATURE_SOCKET_ENABLED", False
-    ):
-        abort(404)
+    # if request.path.startswith("/some-feature") and not current_app.config.get("FEATURE_SOME_FEATURE_ENABLED", False):
+    #     abort(404)
     pass
 
 
