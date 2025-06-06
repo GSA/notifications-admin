8c1c9e877db8a0a43cd504817bf0ed93ab28d237
Kenneth Kehl <@kkehl@flexion.us>
2025-06-06 10:47:38 -0700

    fix api health

--- app/utils/api_health.py
+++ app/utils/api_health.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import ssl
 
 import requests
 from requests.exceptions import RequestException
@@ -7,10 +8,18 @@ from requests.exceptions import RequestException
 logger = logging.getLogger(__name__)
 
 
+def get_no_x509_strict_context():
+    context = ssl.create_default_context()
+    context.options |= ssl.OP_NO_X509_STRICT
+    return context
+
+
 def is_api_down():
     api_base_url = os.getenv("API_HOST_NAME")
     try:
-        response = requests.get(api_base_url, timeout=2)
+        response = requests.get(
+            api_base_url, timeout=2, cert=get_no_x509_strict_context()
+        )
         is_down = response.status_code != 200
         if is_down:
             logger.warning(
