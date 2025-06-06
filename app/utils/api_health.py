c5613954a03df74d47da7f7ffd6abdc3ec455196
Kenneth Kehl <@kkehl@flexion.us>
2025-06-06 11:03:43 -0700

    fix api health

--- app/utils/api_health.py
+++ app/utils/api_health.py
@@ -18,7 +18,7 @@ def is_api_down():
     api_base_url = os.getenv("API_HOST_NAME")
     try:
         response = requests.get(
-            api_base_url, timeout=2, cert=get_no_x509_strict_context()
+            api_base_url, timeout=2, verify=get_no_x509_strict_context()
         )
         is_down = response.status_code != 200
         if is_down:
