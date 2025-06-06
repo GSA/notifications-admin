3c63a345c91f77367950d043e55b1ad48d8b20d4
Kenneth Kehl <@kkehl@flexion.us>
2025-06-06 11:18:47 -0700

    fix api health

--- app/utils/api_health.py
+++ app/utils/api_health.py
@@ -7,7 +7,7 @@ from requests.exceptions import RequestException
 
 logger = logging.getLogger(__name__)
 
-
+# Is this right and can we use it anywhere?
 def get_no_x509_strict_context():
     context = ssl.create_default_context()
     context.verify_flags &= ~ssl.VERIFY_X509_STRICT
@@ -18,7 +18,7 @@ def is_api_down():
     api_base_url = os.getenv("API_HOST_NAME")
     try:
         response = requests.get(
-            api_base_url, timeout=2, verify=get_no_x509_strict_context()
+            api_base_url, timeout=2, verify=False
         )
         is_down = response.status_code != 200
         if is_down:
