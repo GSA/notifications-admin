97c1093de720194356497be151efdcd8ce7b4003
Beverly Nguyen <Beverly.Nguyen@gsa.gov>
2025-06-11 14:09:17 -0700

    dynamically update title and page descriptions

--- app/assets/javascripts/templateFolderForm.js
+++ app/assets/javascripts/templateFolderForm.js
@@ -294,11 +294,23 @@
       if (this.currentState === 'add-new-template') {
         this.$form.find('.template-list-item').addClass('js-hidden');
         $('.live-search').addClass('js-hidden');
+        $('#breadcrumb-template-folders').addClass('js-hidden');
+        $('#template-list').addClass('js-hidden');
         this.$form.find('input[type=checkbox]').prop('checked', false);
         this.selectionStatus.update({ total: 0, templates: 0, folders: 0 });
+
+        $('#page-title').text('New Template');
+        $('#page-description').text('Every message starts with a template. Select to start with a blank template or copy an existing template.');
+        document.title = 'New Templates';
       } else {
         this.$form.find('.template-list-item').removeClass('js-hidden');
         $('.live-search').removeClass('js-hidden');
+        $('#breadcrumb-template-folders').removeClass('js-hidden');
+        $('#template-list').removeClass('js-hidden');
+
+        $('#page-title').text('Select or create a template');
+        $('#page-description').text('Every message starts with a template. To send, choose or create a template.');
+        document.title = 'Select or create a template';
       }
 
       if (currentStateObj && 'setFocus' in currentStateObj) {
