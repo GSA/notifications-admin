5e4e763119017e44f450e59aab8121bbde52305c
Carlo Costino <carlo.costino@gsa.gov>
2025-07-31 12:55:25 -0400

    Remove logging from gunicorn config file
    This changeset removes the logging statements in the gunicorn config file to see if that addresses the issue we are seeing.
    
    Signed-off-by: Carlo Costino <carlo.costino@gsa.gov>

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,7 +2,6 @@ import multiprocessing
 import os
 import sys
 import traceback
-import logging
 from app.utils import hilite
 import gunicorn
 
@@ -13,8 +12,6 @@ import gunicorn
 workers = multiprocessing.cpu_count()
 worker_class = "gevent"
 
-logging.basicConfig(level=logging.INFO)
-logging.info(hilite("Gunicorn timeout set to 240 seconds"))
 timeout = 240
 bind = "0.0.0.0:{}".format(os.getenv("PORT"))
 disable_redirect_access_to_syslog = True
