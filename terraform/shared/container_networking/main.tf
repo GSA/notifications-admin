6838f94c7ab2f2b372abcd2e885eca15b06cf600
Kenneth Kehl <@kkehl@flexion.us>
2025-10-15 10:34:38 -0700

    fiddle with container networking

--- terraform/shared/container_networking/main.tf
+++ terraform/shared/container_networking/main.tf
@@ -1,20 +1,21 @@
 data "cloudfoundry_space" "space" {
-  org_name = var.cf_org_name
-  name     = var.cf_space_name
+
+  name = var.cf_space_name
 }
 
 data "cloudfoundry_app" "source_app" {
-  name_or_id = var.source_app_name
-  space      = data.cloudfoundry_space.space.id
+  org_name   = var.cf_org_name
+  name       = var.source_app_name
+  space_name = data.cloudfoundry_space.space.id
 }
 
 data "cloudfoundry_app" "destination_app" {
-  name_or_id = var.destination_app_name
-  space      = data.cloudfoundry_space.space.id
+  name       = var.destination_app_name
+  space_name = data.cloudfoundry_space.space.id
 }
 
 resource "cloudfoundry_network_policy" "internal_route" {
-  policy {
+  policies {
     source_app      = data.cloudfoundry_app.source_app.id
     destination_app = data.cloudfoundry_app.destination_app.id
     port            = var.destination_port
