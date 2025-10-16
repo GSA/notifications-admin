9941170ad68770f5806278523898c2cb011616bd
Kenneth Kehl <@kkehl@flexion.us>
2025-10-16 13:33:36 -0700

    try pointing modules to old provider

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -30,9 +30,6 @@ module "logo_upload_bucket" {
 
   cf_space_id = local.cf_space_name
   name        = "${local.app_name}-logo-upload-bucket-${local.env}"
-  providers = {
-    cloudfoundry-community = cloudfoundry-community.legacy
-  }
 }
 
 module "api_network_route" {
