472192c29d182c8f3e5a08761faa135cf34db806
Kenneth Kehl <@kkehl@flexion.us>
2025-10-20 10:33:16 -0700

    fix provider name

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -27,8 +27,11 @@ module "redis-v70" {
 }
 
 module "logo_upload_bucket" {
-  source = "github.com/GSA-TTS/terraform-cloudgov//s3?ref=v1.0.0"
-
+  source = "github.com/GSA-TTS/terraform-cloudgov//s3?ref=v2.4.0"
+  # Right now the default is cfcommunity, remove this when default is cloudfoundry
+  providers = {
+    cloudfoundry = cloudfoundry.official
+  }
   cf_org_name   = local.cf_org_name
   cf_space_name = local.cf_space_name
   name          = "${local.app_name}-logo-upload-bucket-${local.env}"
@@ -36,10 +39,7 @@ module "logo_upload_bucket" {
 
 module "api_network_route" {
   source = "../shared/container_networking"
-  # Right now the default is cfcommunity, remove this when default is cloudfoundry
-  providers = {
-    cloudfoundry = cloudfoundry.official
-  }
+
   cf_org_name          = local.cf_org_name
   cf_space_name        = local.cf_space_name
   source_app_name      = "${local.app_name}-${local.env}"
