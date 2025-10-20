4020f61430ebed9defe0d737d6363f4db7c77c7e
Kenneth Kehl <@kkehl@flexion.us>
2025-10-20 12:36:59 -0700

    fix provider name

--- terraform/staging/providers.tf
+++ terraform/staging/providers.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     cloudfoundry = {
       source  = "cloudfoundry/cloudfoundry"
-      version = "1.11.1"
+      version = "1.11.0"
     }
     cfcommunity = {
       source  = "cloudfoundry-community/cloudfoundry"
