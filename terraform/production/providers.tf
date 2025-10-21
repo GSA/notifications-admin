b0adac419a0773b839e2fc5a28a5d40b8afef9f5
Kenneth Kehl <@kkehl@flexion.us>
2025-10-21 09:43:06 -0700

    fix tfstate file names

--- terraform/production/providers.tf
+++ terraform/production/providers.tf
@@ -13,7 +13,7 @@ terraform {
 
   backend "s3" {
     bucket       = "cg-6b759c13-6253-4a64-9bda-dd1f620185b0"
-    key          = "admin.tfstate.stage"
+    key          = "admin.tfstate.prod"
     encrypt      = "true"
     region       = "us-gov-west-1"
     use_lockfile = "true"
