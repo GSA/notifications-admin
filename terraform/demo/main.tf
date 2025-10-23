fce794f24916c018697513e3f1e9129e467034a0
Kenneth Kehl <@kkehl@flexion.us>
2025-10-23 07:19:26 -0700

    destroy modules using deprecated provider

--- terraform/demo/main.tf
+++ terraform/demo/main.tf
@@ -12,33 +12,33 @@ resource "null_resource" "prevent_destroy" {
   }
 }
 
-module "redis-v70" {
-  source = "github.com/GSA-TTS/terraform-cloudgov//redis?ref=v1.0.0"
+# module "redis-v70" {
+# source = "github.com/GSA-TTS/terraform-cloudgov//redis?ref=v1.0.0"
 
-  cf_org_name     = local.cf_org_name
-  cf_space_name   = local.cf_space_name
-  name            = "${local.app_name}-redis-v70-${local.env}"
-  redis_plan_name = "redis-dev"
-  json_params = jsonencode(
-    {
-      "engineVersion" : "7.0",
-    }
-  )
-}
+# cf_org_name     = local.cf_org_name
+# cf_space_name   = local.cf_space_name
+# name            = "${local.app_name}-redis-v70-${local.env}"
+# redis_plan_name = "redis-dev"
+# json_params = jsonencode(
+# {
+# "engineVersion" : "7.0",
+# }
+# )
+# }
 
-module "logo_upload_bucket" {
-  source = "github.com/GSA-TTS/terraform-cloudgov//s3?ref=v1.0.0"
+# module "logo_upload_bucket" {
+# source = "github.com/GSA-TTS/terraform-cloudgov//s3?ref=v1.0.0"
 
-  cf_org_name   = local.cf_org_name
-  cf_space_name = local.cf_space_name
-  name          = "${local.app_name}-logo-upload-bucket-${local.env}"
-}
+# cf_org_name   = local.cf_org_name
+# cf_space_name = local.cf_space_name
+# name          = "${local.app_name}-logo-upload-bucket-${local.env}"
+# }
 
-module "api_network_route" {
-  source = "../shared/container_networking"
+# module "api_network_route" {
+# source = "../shared/container_networking"
 
-  cf_org_name          = local.cf_org_name
-  cf_space_name        = local.cf_space_name
-  source_app_name      = "${local.app_name}-${local.env}"
-  destination_app_name = "notify-api-${local.env}"
-}
+# cf_org_name          = local.cf_org_name
+# cf_space_name        = local.cf_space_name
+# source_app_name      = "${local.app_name}-${local.env}"
+# destination_app_name = "notify-api-${local.env}"
+# }
